ad0e4eea22f2d5b444394b638b3745563e3c2bda
Johan Wirén <johan.wiren.se@gmail.com>
2019-12-02 22:44:33 +0100

    Day 3 prep

---
+++ src/adventofcode_2019/day_3.clj
@@ -0,0 +1,32 @@
+(ns adventofcode-2019.day-3
+  (:require [adventofcode-2019.util :refer [slurp-input slurp-reference-input]]
+            [clojure.edn :as edn]
+            [clojure.string :as str]
+            [clojure.test :as t]))
+
+(def reference-input (slurp-reference-input))
+
+(def input (slurp-input))
+
+(defn parse-input [in]
+  in)
+
+(defn part-1-solver [in]
+  in)
+
+(defn part-2-solver [in]
+  in)
+
+(comment
+
+  (part-1-solver input)
+
+  (part-2-solver input)
+
+  )
+
+(t/deftest part-1-test
+  (t/is (= :FIXME (part-1-solver reference-input))))
+
+(t/deftest part-2-test
+  (t/is (= :FIXME (part-2-solver reference-input))))
